33314872b9abbac01f041bf00ad637559b260bb8
“ShivamLekhwar” <shivam.lekhwar@loconav.com>
2019-07-07 15:46:57 +0530

    after postgre

--- js/ex2/ex2_jsfile.js
+++ js/ex2/ex2_jsfile.js
@@ -1,78 +1,84 @@
 /* eslint-disable no-alert */
 // TODO[Gaurav] No need to use static methods here. Use all the methods and variables as object level.
 class MyCheckBox {
-  constructor(checks, noneCheck, maxSelection) {
-    this.checks = checks
-    this.maxSelection = maxSelection
-    this.dayCount = 0
+    constructor(checks, noneCheck, maxSelection) {
+        this.checks = checks
+        this.maxSelection = maxSelection
+        this.dayCount = 0
 
-    for (let i = 0; i < this.checks.length; i += 1) {
-    // eslint-disable-next-line func-names
-    // TODO[gaurav] Mainfunction name change
-      this.checks[i].addEventListener('click',this.dayCheckBoxHandler.bind(this))
+        for (let i = 0; i < this.checks.length; i += 1) {
+            // eslint-disable-next-line func-names
+            // TODO[gaurav] Mainfunction name change
+            this.checks[i].addEventListener(
+                'click',
+                this.dayCheckBoxHandler.bind(this)
+            )
+        }
+
+        this.noneCheck = noneCheck
+        this.noneCheck.addEventListener(
+            'click',
+            this.handleNoneCheckClick.bind(this)
+        )
     }
 
-    this.noneCheck = noneCheck
-    this.noneCheck.addEventListener('click',this.handleNoneCheckClick.bind(this))
-  }
-
-  increaseDayCount() {
-    this.dayCount = this.dayCount + 1
-  }
-
-  decreaseDayCount() {
-    this.dayCount = this.dayCount - 1
-  }
-
-  resetDayCount() {
-    this.dayCount = 0
-  }
-
-  getCount() {
-    return this.dayCount
-  }
-
-
-  printMsg() {
-    let string = ""
-    for (let i = 0; i<this.checks.length; i += 1) {
-      if (this.checks[i].checked === true){
-        string = string + " " + this.checks[i].name
-      }
+    increaseDayCount() {
+        this.dayCount = this.dayCount + 1
     }
-    alert(`Only ${this.maxSelection} days can be selected You have already selected ${string} `)
-  }
 
-
-  handleNoneCheckClick() {
-    if (this.checked !== false) {
-      for (let i = 0; i < this.checks.length; i += 1) {
-      // eslint-disable-next-line func-names
-        this.checks[i].checked = false
-        this.resetDayCount()
-      }
+    decreaseDayCount() {
+        this.dayCount = this.dayCount - 1
     }
-  }
 
-  dayCheckBoxHandler() {
-    if (event.target.checked === false) {
-      this.decreaseDayCount()
-    } else {
-      this.noneCheck.checked = false
-      if (this.getCount() == this.maxSelection) {
-        event.target.checked = false
-        this.printMsg()
-      } else {
-        this.increaseDayCount()
-      }
+    resetDayCount() {
+        this.dayCount = 0
     }
-  }
+
+    getCount() {
+        return this.dayCount
+    }
+
+    printMsg() {
+        let string = ''
+        for (let i = 0; i < this.checks.length; i += 1) {
+            if (this.checks[i].checked === true) {
+                string = string + ' ' + this.checks[i].name
+            }
+        }
+        alert(
+            `Only ${this.maxSelection} days can be selected You have already selected ${string} `
+        )
+    }
+
+    handleNoneCheckClick() {
+        if (this.checked !== false) {
+            for (let i = 0; i < this.checks.length; i += 1) {
+                // eslint-disable-next-line func-names
+                this.checks[i].checked = false
+                this.resetDayCount()
+            }
+        }
+    }
+
+    dayCheckBoxHandler() {
+        if (event.target.checked === false) {
+            this.decreaseDayCount()
+        } else {
+            this.noneCheck.checked = false
+            if (this.getCount() == this.maxSelection) {
+                event.target.checked = false
+                this.printMsg()
+            } else {
+                this.increaseDayCount()
+            }
+        }
+    }
 }
 
 const MyCheckBoxObject = new MyCheckBox(
-  document.getElementsByClassName('day'),
-  document.getElementById('none'),
-  3
+    document.getElementsByClassName('day'),
+    document.getElementById('none'),
+    3
 )
 
 // const checks = document.getElementsByClassName('meri')
